2dcb6c527b4cd9a8fbb002701840bbecd9f36fd6
Brian Wallen <bwallen@gmail.com>
2024-01-30 14:06:12 -0500

    More tile layers and overlays, fix for markers with PNG icons

--- src/pages/Map/Map.tsx
+++ src/pages/Map/Map.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { renderToString } from 'react-dom/server';
-import { LayersControl, MapContainer, ScaleControl, TileLayer, useMap } from 'react-leaflet';
+import { LayersControl, MapContainer, ScaleControl, TileLayer, useMap, WMSTileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import { FullscreenControl } from 'react-leaflet-fullscreen';
 import L from 'leaflet';
@@ -49,7 +49,7 @@ export default function Map() {
             if (eud.last_point !== null) {
                 const marker = L.marker(
                     [eud.last_point.latitude, eud.last_point.longitude],
-                    {icon: arrowIcon, rotationOrigin: 'center center'});
+                    { icon: arrowIcon, rotationOrigin: 'center center' });
                 marker.bindPopup(description);
                 marker.bindTooltip(eud.callsign, {
                     opacity: 0.7,
@@ -97,6 +97,7 @@ export default function Map() {
             }
 
             function onMarker(value: any) {
+                console.log(value);
                 const { uid } = value;
 
                 if (Object.hasOwn(value, 'iconset_path') &&
@@ -130,8 +131,12 @@ export default function Map() {
                         offset: [12, 35],
                     });
 
-                    if (value.mil_std_2525c !== null) {
-                        const symbol = new milsymbol.default.Symbol(value.mil_std_2525c, { size: 25 });
+                    if (value.mil_std_2525c !== null && !value.iconset_path.endsWith('.png')) {
+                        const options = { size: 25, direction: undefined };
+                        if (value.point !== null && value.point.course !== null) {
+                            options.direction = value.point.course;
+                        }
+                        const symbol = new milsymbol.default.Symbol(value.mil_std_2525c, options);
                         marker.setIcon(L.divIcon({
                             className: '',
                             html: symbol.asSVG(),
@@ -223,7 +228,7 @@ export default function Map() {
               center={[10, 0]}
               zoom={3}
               scrollWheelZoom
-              style={{ height: 'calc(100vh - 10rem)', width: '100%', zIndex: 125 }}
+              style={{ height: 'calc(100vh - 10rem)', width: '100%', zIndex: 90 }}
             >
                 <MapContext />
                 <ScaleControl />
@@ -233,18 +238,48 @@ export default function Map() {
                         <TileLayer
                           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                          zIndex={0}
                         />
                     </LayersControl.BaseLayer>
+                    <LayersControl.BaseLayer name="Google Streets">
+                        <TileLayer url="http://mt0.google.com/vt/lyrs=m&x={x}&y={y}&z={z}" zIndex={0} />
+                    </LayersControl.BaseLayer>
                     <LayersControl.BaseLayer name="Google Hybrid">
-                        <TileLayer url="http://mt0.google.com/vt/lyrs=y&hl=en&x={x}&y={y}&z={z}&s=Ga" />
+                        <TileLayer url="http://mt0.google.com/vt/lyrs=y&hl=en&x={x}&y={y}&z={z}&s=Ga" zIndex={0} />
+                    </LayersControl.BaseLayer>
+                    <LayersControl.BaseLayer name="Google Terrain">
+                        <TileLayer url="http://mt1.google.com/vt/lyrs=p&amp;x={x}&amp;y={y}&amp;z={z}" zIndex={0} />
+                    </LayersControl.BaseLayer>
+                    <LayersControl.BaseLayer name="ESRI World Imagery (Clarity) Beta">
+                        <TileLayer url="http://clarity.maptiles.arcgis.com/arcgis/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}" />
+                    </LayersControl.BaseLayer>
+                    <LayersControl.BaseLayer name="ESRI World Topo">
+                        <TileLayer url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}" />
                     </LayersControl.BaseLayer>
                 </LayersControl>
                 <LayersControl position="topright">
-                    <LayersControl.Overlay name="Street Maps Coverage">
+                    <LayersControl.Overlay name="Google Street View Coverage">
                         <TileLayer
                           url="https://www.google.com/maps/vt?pb=!1m7!8m6!1m3!1i{z}!2i{x}!3i{y}!2i9!3x1!2m8!1e2!2ssvv!4m2!1scc!2s*211m3*211e2*212b1*213e2*212b1*214b1!4m2!1ssvl!2s*211b0*212b1!3m8!2sen!3sus!5e1105!12m4!1e68!2m2!1sset!2sRoadmap!4e0!5m4!1e0!8m2!1e1!1e1!6m6!1e12!2i2!11e0!39b0!44e0!50e0"
-                          zIndex={9999}
+                          pane="overlayPane"
                         />
+                    </LayersControl.Overlay>
+                    <LayersControl.Overlay name="Weather">
+                        <WMSTileLayer
+                          url="http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi"
+                          params={{
+                            layers: 'nexrad-n0r-900913',
+                            format: 'image/png',
+                            transparent: true }}
+                          attribution="Weather data © 2012 IEM Nexrad"
+                          pane="overlayPane"
+                        />
+                    </LayersControl.Overlay>
+                    <LayersControl.Overlay name="Google Roads Overlay">
+                        <TileLayer url="http://mt1.google.com/vt/lyrs=h&amp;x={x}&amp;y={y}&amp;z={z}" pane="overlayPane" />
+                    </LayersControl.Overlay>
+                    <LayersControl.Overlay name="Google Terrain Overlay">
+                        <TileLayer url="http://mt1.google.com/vt/lyrs=t&amp;x={x}&amp;y={y}&amp;z={z}" pane="overlayPane" />
                     </LayersControl.Overlay>
                 </LayersControl>
             </MapContainer>
