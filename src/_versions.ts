2ed0b81019a8bc1c773f21664b19bb4b6693a02d
Brian Wallen <bwallen@gmail.com>
2024-10-17 20:08:05 -0400

    Updated eslint

--- src/_versions.ts
+++ src/_versions.ts
@@ -11,10 +11,10 @@ export interface TsAppVersion {
 export const versions: TsAppVersion = {
     version: '0.0.0',
     name: 'opentakserver-ui',
-    versionDate: '2024-10-17T21:51:35.874Z',
-    gitCommitHash: 'g1fb2ee8',
-    gitCommitDate: '2024-10-16T12:44:43.000Z',
-    versionLong: '0.0.0-g1fb2ee8',
+    versionDate: '2024-10-18T00:02:52.297Z',
+    gitCommitHash: 'g95b3372',
+    gitCommitDate: '2024-10-17T22:39:50.000Z',
+    versionLong: '0.0.0-g95b3372',
     gitTag: 'v1.3.0',
 };
 export default versions;
