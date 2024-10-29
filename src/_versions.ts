cc10826f841d54968ac3aea2634f8df763ca3fdc
Brian Wallen <bwallen@gmail.com>
2024-10-28 23:32:07 -0400

    Added thumbnails to the Video Streams and Recordings pages

--- src/_versions.ts
+++ src/_versions.ts
@@ -11,10 +11,10 @@ export interface TsAppVersion {
 export const versions: TsAppVersion = {
     version: '0.0.0',
     name: 'opentakserver-ui',
-    versionDate: '2024-10-18T03:22:38.216Z',
-    gitCommitHash: 'gbc7a9ba',
-    gitCommitDate: '2024-10-18T01:32:43.000Z',
-    versionLong: '0.0.0-gbc7a9ba',
-    gitTag: 'v1.3.0',
+    versionDate: '2024-10-29T03:23:42.940Z',
+    gitCommitHash: 'g3a0ace3',
+    gitCommitDate: '2024-10-22T13:48:35.000Z',
+    versionLong: '0.0.0-g3a0ace3',
+    gitTag: 'v1.4.0rc1',
 };
 export default versions;
