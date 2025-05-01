3a6b7ec0887851a40d4686222c21e5743469c803
Brian Wallen <bwallen@gmail.com>
2025-05-01 09:17:31 -0400

    Bumped Mantine to 7.17.5, added semver-parser

--- src/_versions.ts
+++ src/_versions.ts
@@ -11,10 +11,10 @@ export interface TsAppVersion {
 export const versions: TsAppVersion = {
     version: '0.0.0',
     name: 'opentakserver-ui',
-    versionDate: '2025-04-29T19:59:36.878Z',
-    gitCommitHash: 'g3a91982',
-    gitCommitDate: '2025-04-24T04:27:12.000Z',
-    versionLong: '0.0.0-g3a91982',
+    versionDate: '2025-05-01T13:11:33.229Z',
+    gitCommitHash: 'gf254a24',
+    gitCommitDate: '2025-04-29T20:04:03.000Z',
+    versionLong: '0.0.0-gf254a24',
     gitTag: 'v1.5.0rc1',
 };
 export default versions;
