f881bd912a1d1e6ee52746da3510a5a391f24a99
Brian <bwallen@gmail.com>
2025-08-14 11:01:31 -0400

    Fixed yarn, bumped dependencies

--- src/_versions.ts
+++ src/_versions.ts
@@ -11,10 +11,10 @@ export interface TsAppVersion {
 export const versions: TsAppVersion = {
     version: '0.0.0',
     name: 'opentakserver-ui',
-    versionDate: '2025-08-14T13:45:58.029Z',
-    gitCommitHash: 'gb78d377',
-    gitCommitDate: '2025-08-14T13:36:38.000Z',
-    versionLong: '0.0.0-gb78d377',
+    versionDate: '2025-08-14T14:55:43.106Z',
+    gitCommitHash: 'g41c443a',
+    gitCommitDate: '2025-08-14T13:48:08.000Z',
+    versionLong: '0.0.0-g41c443a',
     gitTag: 'v1.5.1',
 };
 export default versions;
